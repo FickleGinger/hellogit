2b5f9b6786bbbc293af16c5d033d16e2669bb671
tom_glo <goodMorning_glo@qq.com>
2020-09-24 00:18:45 +0800

    分支添加

--- HelloGit/src/main/java/com/czq/git/HelloGit.java
+++ HelloGit/src/main/java/com/czq/git/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
         System.out.println("Hello Git!!!!");
         System.out.println("更新！");
         System.out.println("创建分支");
-        System.out.println("逐渐添加");
+        System.out.println("主干添加");
         System.out.println("分支添加");
     }
 }
